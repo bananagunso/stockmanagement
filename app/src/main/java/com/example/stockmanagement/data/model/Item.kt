741c7e3dd0b4d0c29b905fbad62347ec39346985
Isamu Naojima <naojima@eigahan.com>
2026-09-07 17:11:55 +0900

    item一覧にcategoryも表示

--- app/src/main/java/com/example/stockmanagement/data/model/Item.kt
+++
@@ -1,8 +0,0 @@
-package com.example.stockmanagement.data.model
-
-data class Item(
-    val id: Long,
-    val name: String,
-    val category: String,
-    val quantity: Int
-)
---
+++ app/src/main/java/com/example/stockmanagement/data/model/ItemWithCategory.kt
@@ -0,0 +1,8 @@
+package com.example.stockmanagement.data.model
+
+data class AttributeWithDataType(
+    val attributeId: Int,
+    val name: String,
+    val dataTypeId: Int,
+    val dataTypeName: String
+)
